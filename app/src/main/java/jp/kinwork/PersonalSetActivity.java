d89184d2b355ca38618c6d297bd320d4a693bed5
choho950911 <64938883+choho950911@users.noreply.github.com>
2020-06-23 15:18:08 +0900

    Update PersonalSetActivity.java

--- app/src/main/java/jp/kinwork/PersonalSetActivity.java
+++ app/src/main/java/jp/kinwork/PersonalSetActivity.java
@@ -5,7 +5,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.net.sip.SipSession;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -16,27 +15,13 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.gson.Gson;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
-
-import jp.kinwork.Common.AES;
-import jp.kinwork.Common.AESprocess;
 import jp.kinwork.Common.MyApplication;
-import jp.kinwork.Common.NetworkUtils;
-import jp.kinwork.Common.PostDate;
+
 import jp.kinwork.Common.PreferenceUtils;
 
 public class PersonalSetActivity extends AppCompatActivity {
 
-    final static String PARAM_File = "/MyMailMobile/getDialogList";
 
     private TextView tvtitle;
     private TextView tvResumeSet1;
@@ -57,10 +42,6 @@ public class PersonalSetActivity extends AppCompatActivity {
     private TextView tvname;
     private TextView tvemail;
 
-    private String deviceId;
-    private String AesKey;
-    private String userId;
-    private String token;
 
     private MyApplication myApplication;
     private PreferenceUtils PreferenceUtils;
@@ -101,15 +82,7 @@ public class PersonalSetActivity extends AppCompatActivity {
         tvpersonalsettings = (TextView) findViewById(R.id.tv_b_personalsettings);
         ivpersonalsettings.setImageResource(R.mipmap.blue_personalsettings);
         tvpersonalsettings.setTextColor(Color.parseColor("#5EACE2"));
-        deviceId = PreferenceUtils.getdeviceId();
-        AesKey = PreferenceUtils.getAesKey();
-        userId = PreferenceUtils.getuserId();
-        token = PreferenceUtils.gettoken();
         myApplication = (MyApplication) getApplication();
-        if(myApplication.getlast_name().length() > 0){
-            tvname.setText(myApplication.getlast_name() + myApplication.getfirst_name() + " 様");
-            urllodad();
-        }
         PreferenceUtils = new PreferenceUtils(PersonalSetActivity.this);
     }
     //菜单栏按钮
@@ -161,74 +134,6 @@ public class PersonalSetActivity extends AppCompatActivity {
                 break;
         }
         startActivity(intent);
-    }
-
-    //内容取得、通信
-    private void urllodad() {
-        //Json格式转换并且加密
-        PostDate Pdata = new PostDate();
-        Pdata.setUserId(userId);
-        Pdata.setToken(token);
-        Gson mGson1 = new Gson();
-        String sdPdata = mGson1.toJson(Pdata,PostDate.class);
-        Log.d("***mailTitle***", sdPdata);
-        AES mAes = new AES();
-        byte[] mBytes = null;
-        try {
-            mBytes = sdPdata.getBytes("UTF8");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        String enString = mAes.encrypt(mBytes,AesKey);
-        String data = enString.replace("\n", "").replace("+","%2B");
-
-        Map<String,String> param = new HashMap<String, String>();
-        PreferenceUtils.setdeviceId(deviceId);
-        PreferenceUtils.setAesKey(AesKey);
-        param.put(getString(R.string.file),PARAM_File);
-        param.put(getString(R.string.data),data);
-        //数据通信处理（访问服务器，并取得访问结果）
-        new GithubQueryTask().execute(param);
-    }
-
-    //访问服务器，并取得访问结果
-    private class GithubQueryTask extends AsyncTask<Map<String, String>, Void, String> {
-
-        @Override
-        protected String doInBackground(Map<String, String>... params) {
-            Map<String, String> map = params[0];
-            String file = map.get(getString(R.string.file));
-            String data = map.get(getString(R.string.data));
-            URL searchUrl = NetworkUtils.buildUrl(file);
-            String githubSearchResults = null;
-            try {
-                githubSearchResults = NetworkUtils.getResponseFromHttpUrl(searchUrl,data,deviceId);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            return githubSearchResults;
-        }
-        @Override
-        protected void onPostExecute(String githubSearchResults) {
-            if (githubSearchResults != null && !githubSearchResults.equals("")) {
-                Log.d("***Results***", githubSearchResults);
-                try {
-                    JSONObject obj = new JSONObject(githubSearchResults);
-                    Boolean processResult = obj.getBoolean(getString(R.string.processResult));
-                    String message = obj.getString(getString(R.string.message));
-                    if(processResult == true) {
-                        String returnData = obj.getString(getString(R.string.returnData));
-                    } else {
-                        alertDialog(message);
-                    }
-                }catch (Exception e){
-                    e.printStackTrace();
-                }
-            }else {
-
-            }
-        }
     }
 
     //通信结果提示
